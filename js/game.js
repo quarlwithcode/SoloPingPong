28ba063a36a6020ae68394de414df41ab9286818
Vincent Quarles <soulcaught@gmail.com>
2016-06-28 14:49:04 -0400

    Week 02 changes
    Created Canvas
    Input for mouse x and y position
    
    Starting to build solo pong game

--- js/game.js
+++ js/game.js
@@ -19,4 +19,42 @@ window.cancelRequestAnimFrame = ( function() {
 		clearTimeout;
 } )();
 
-console.log('Holla');
+//DO NOT TOUCH CODE ABOVE
+
+//console.log('Holla');
+
+//Step 01 .. vnq .. Create game cnavas and track mouse position
+var gameCanvas = document.getElementById("canvas");         //Store HTML5 canvas tag into JS variable
+
+var ctx = gameCanvas.getContext("2d");          //Creates context 2D
+
+//Window Size Constants
+var W = window.innerWidth;
+var H = window.innerHeight;
+
+
+var mouseObj = {};
+
+gameCanvas.width = W;
+gameCanvas.height = H;
+
+function paintCanvas(){
+    ctx.fillStyle = "#2c3e50";
+    ctx.fillRect(0, 0, W, H);
+    
+    
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "50px sans-serif";
+    ctx.fillText("Hello World!", 10, 50);
+}
+
+paintCanvas();
+
+
+function trackPosition(evt){
+    mouseObj.x = evt.pageX;
+    mouseObj.y = evt.pageY;
+    console.log("Cursor X is: " + mouseObj.x + "\nCursor Y is: " + mouseObj.y);
+}
+
+gameCanvas.addEventListener("mousemove", trackPosition, true);
