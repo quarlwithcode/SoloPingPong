5b8244e90720353b748d8386f3085a7f41854bb2
Vincent Quarles <soulcaught@gmail.com>
2016-07-12 12:39:03 -0400

    Adding Sound Files
    Instructor said so.

--- js/game.js
+++ js/game.js
@@ -18,43 +18,96 @@ window.cancelRequestAnimFrame = ( function() {
 			window.msCancelRequestAnimationFrame     ||
 		clearTimeout;
 } )();
+// Do No Touch Above Code
 
-//DO NOT TOUCH CODE ABOVE
 
-//console.log('Holla');
 
-//Step 01 .. vnq .. Create game cnavas and track mouse position
-var gameCanvas = document.getElementById("canvas");         //Store HTML5 canvas tag into JS variable
+// ### Step 01 ..tat.. Create game canvas and track mouse position ###
 
-var ctx = gameCanvas.getContext("2d");          //Creates context 2D
+var gameCanvas = document.getElementById("canvas");
+// Store HTML5 canvas tag into JS variable
 
-//Window Size Constants
+var ctx = gameCanvas.getContext("2d"); // Create context 2D
 var W = window.innerWidth;
 var H = window.innerHeight;
-
 
 var mouseObj = {};
 
 gameCanvas.width = W;
 gameCanvas.height = H;
 
-function paintCanvas(){
-    ctx.fillStyle = "#2c3e50";
-    ctx.fillRect(0, 0, W, H);
-    
-    
-    ctx.fillStyle = "#FFFFFF";
-    ctx.font = "50px sans-serif";
-    ctx.fillText("Hello World!", 10, 50);
-}
 
+// Step 02 ..tat.. Clear page canvas by covering it in black
+function paintCanvas() {
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, W, H);
+}
 paintCanvas();
 
 
-function trackPosition(evt){
+function trackPosition(evt) {
     mouseObj.x = evt.pageX;
     mouseObj.y = evt.pageY;
-    console.log("Cursor X is: " + mouseObj.x + "\nCursor Y is: " + mouseObj.y);
+    console.log("Cursor x is: " + mouseObj.x + " Cursor y is: " + mouseObj.y);
 }
-
 gameCanvas.addEventListener("mousemove", trackPosition, true);
+
+
+// Step 03 ..tat.. Place a ball on the canvas
+var ball = {}; // Ball Object
+ball = {
+    x: 50,
+    y: 50,
+    r: 15,
+    c: "#ffffff",
+    vx: 4,
+    vy: 8,
+    
+    draw : function() {
+        ctx.beginPath();
+        ctx.fillStyle = this.c;
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+        ctx.fill();
+    }
+}
+ball.draw();
+
+// Step 04 ..tat.. Place a start button on the canvas
+var startBtn = {}; //Start button object
+startBtn = {
+    w: 100,
+    h: 50,
+    x: W / 2 - 50,
+    y: H / 2 - 25,
+    
+    draw: function() {
+        ctx.strokeStyle = "#ffffff";
+        ctx.lineWidth = "2";
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+        
+        ctx.font = "18px Arial, sans-serif";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText("Start", W / 2, H / 2);
+    }
+}
+startBtn.draw();
+
+// Step 05 ..tat.. Place score and points on canvas
+var points = 0; // Game Points
+function paintScore() {
+    ctx.fillStyle = '#FFFFFF';
+    ctx.font = "18px Arial, sans-serif";
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'top';
+    ctx.fillText('Score: ' + points, 20, 20);
+}
+paintScore();
+
+
+
+
+
+
+
