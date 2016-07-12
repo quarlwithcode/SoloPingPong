43466fddb64f7d22453fbf18790f1897adac8449
Vincent Quarles <soulcaught@gmail.com>
2016-07-12 15:01:10 -0400

    Added Collision and Sound FX
    Instructor said to.

--- js/game.js
+++ js/game.js
@@ -22,7 +22,7 @@ window.cancelRequestAnimFrame = ( function() {
 
 
 
-// ### Step 01 ..tat.. Create game canvas and track mouse position ###
+// Step 01 ..vnq.. Create game canvas and track mouse position
 
 var gameCanvas = document.getElementById("canvas");
 // Store HTML5 canvas tag into JS variable
@@ -37,7 +37,7 @@ gameCanvas.width = W;
 gameCanvas.height = H;
 
 
-// Step 02 ..tat.. Clear page canvas by covering it in black
+// Step 02 ..vnq.. Clear page canvas by covering it in black
 function paintCanvas() {
     ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, W, H);
@@ -53,7 +53,7 @@ function trackPosition(evt) {
 gameCanvas.addEventListener("mousemove", trackPosition, true);
 
 
-// Step 03 ..tat.. Place a ball on the canvas
+// Step 03 ..vnq.. Place a ball on the canvas
 var ball = {}; // Ball Object
 ball = {
     x: 50,
@@ -72,7 +72,7 @@ ball = {
 }
 ball.draw();
 
-// Step 04 ..tat.. Place a start button on the canvas
+// Step 04 ..vnq.. Place a start button on the canvas
 var startBtn = {}; //Start button object
 startBtn = {
     w: 100,
@@ -94,7 +94,7 @@ startBtn = {
 }
 startBtn.draw();
 
-// Step 05 ..tat.. Place score and points on canvas
+// Step 05 ..vnq.. Place score and points on canvas
 var points = 0; // Game Points
 function paintScore() {
     ctx.fillStyle = '#FFFFFF';
@@ -105,9 +105,148 @@ function paintScore() {
 }
 paintScore();
 
+// Step 06 ..vnq.. Place paddles (top and bottom) on canvas
+
+function paddlePosition(TB){
+    this.w = 150;
+    this.h = 5;
+    
+    this.x = W/2 - this.w/2;
+    
+    if(TB == "top"){
+        this.y = 0;
+    } else {
+        this.y = H - this.h;
+    }
+}
+
+var paddlesArray = []; //Paddles Array
+
+paddlesArray.push(new paddlePosition("top"));
+paddlesArray.push(new paddlePosition("bottom"));
+console.log("Top paddly y is: " + paddlesArray[0].y);
+console.log("Bottom paddly y is: " + paddlesArray[1].y);
 
 
+function paintPaddles(){
+    for(var lp = 0; lp < paddlesArray.length; lp++){
+        p = paddlesArray[lp];
+        
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(p.x, p.y, p.w, p.h);
+    }
+}
 
+paintPaddles();
+
+// Step 07 ..vnq..  Detect when the user clicks on the screen
+
+gameCanvas.addEventListener("mousedown", btnClick, true);
+
+function btnClick(evt){
+    var mx = evt.pageX;
+    var my = evt.pageY;
+    
+    // User clicked on start button
+    if(mx >= startBtn.x && mx <= startBtn.x + startBtn.w){
+        if(my >= startBtn.y && my <= startBtn.y + startBtn.h){
+            console.log("jeez");  
+            startBtn = {};
+            
+            animloop();
+        }
+    }
+}
+
+
+//Function for running the whole game animation
+
+var init;
+function animloop(){
+    init = requestAnimFrame(animloop);
+    refreshCanvasFun();
+}
+
+//Step 08 ..vnq.. Draw everything on canvas recursively
+
+function refreshCanvasFun(){
+    paintCanvas();
+    paintPaddles();
+    ball.draw();
+    paintScore();
+    update();
+}
+
+function update(){
+    //move the paddles, track the mouse
+    for (var lp = 0; lp < paddlesArray.length; lp++){
+        p = paddlesArray[lp];
+        p.x = mouseObj.x - p.w / 2; 
+    }
+    //move the ball
+    ball.x += ball.vx;
+    ball.y += ball.vy;
+    
+    //check for balll paddle collision
+    check4collision();
+}
+
+function check4collision(){
+    var pTop = paddlesArray[0];
+    var pBot = paddlesArray[1];
+    
+    if(collides(ball, pTop)){
+        collideAction(ball, pTop);
+    } else if(collides(ball, pBot)){
+        collideAction(ball, pBot);
+    } else {
+//        Ball went off the top or bottom of screen
+        if (ball.y + ball.r > H){
+            // Game over
+            
+        } else if(ball.y < 0){
+            //Game voer
+        }
+        
+        //ball hits the side of the screen
+        if (ball.x + ball.r > W){
+            ball.vx = -ball.vx;
+            ball.x = W - ball.r;
+        } else if(ball.x - ball.r < 0){
+            ball.vx = -ball.vx;
+            ball.x = ball.r;
+        }
+    }
+}
+
+var paddleHit;      //which paddle was hit 0 = top, 1 = bottom
+function collides(b, p){
+    if(b.x + b.r >= p.x && b.x - b.r <= p.x + p.w){
+        if(b.y >= (p.y - p.h) && p.y > 0){
+            paddleHit = 0;
+            return true;
+        } else if(b.y <= p.h && p.y === 0){
+            paddleHit = 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+var collisionSnd = document.getElementById("collide");
+
+
+function collideAction(b, p){
+//    console.log("paddle hit: " + paddleHit);
+    if(collisionSnd){
+        collisionSnd.play();
+    }
+    //reverse velocity
+    ball.vy = -ball.vy;
+    //increment score
+    points++;
+}
 
 
 
